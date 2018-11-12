e47a460fdf79d0ee6e040f746ddce456dfaa687c
Daniel Agans <daniel.agans@llamasoft.com>
2018-11-11 19:27:26 -0500

    settings files fixed

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 # API key for https://app.ipgeolocation.io/
-GEO_API_KEY = '3615042e8672467b9d68284fd96a39e6'
+GEO_API_KEY = ''
 # API key for https://openweathermap.org/
-OPEN_WEATHER_MAP_API_KEY = '185faf9625fc688a53cc627eccb91d89'
+OPEN_WEATHER_MAP_API_KEY = ''
 
 # List of icons following what's laid out on:
 #   https://openweathermap.org/weather-conditions
